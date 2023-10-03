c1d873d49722aaf434f33fc8c2043702d278bf8c
DamianoL <damianolunardo@hotmail.it>
2023-10-03 13:58:41 +0200

    first commit

---
+++ psw.js
@@ -0,0 +1,20 @@
+//nome
+const name = prompt("Inserisci il tuo nome");
+console.log("Inserisci il tuo nome");
+
+//cognome
+const surname = prompt("Inserisci il tuo cognome");
+console.log("Inserisci il tuo cognome");
+
+//colore preferito
+const color = prompt("Inserisci il tuo colore preferito");
+console.log("Inserisci il tuo colore preferito");
+
+// Genera un numero casuale compreso tra 0 e 99
+const randomNumber = Math.round(Math.random() * 99);
+
+// Aggiungi il numero casuale alla fine
+const password = name + surname + color + randomNumber;
+
+// Scrivi il risultato sulla pagina
+document.write("La tua password è: " + password);
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Primo esercizio js
+
+### Consegna:
+Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito. Infine scrivi sulla pagina il risultato della concatenazione
+
+
+
+![JAVASCRIPT](https://img.shields.io/badge/JavaScript-F7DF1E.svg?style=for-the-badge&logo=JavaScript&logoColor=black)
